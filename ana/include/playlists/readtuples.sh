783f2c56a012a6ca58f66c59066f17301d32fb67
Anezka Klustova <anezkak@minervagpvm03.fnal.gov>
2023-01-10 08:18:05 -0600

    Updated readtuples script for the newest MAD.

--- ana/include/playlists/readtuples.sh
+++ ana/include/playlists/readtuples.sh
@@ -2,9 +2,14 @@
 
 #for playlist in minervame1A minervame1B minervame1C minervame1D minervame1E minervame1F minervame1G minervame1L minervame1M minervame1N minervame1O minervame1P
 #for playlist in minervame5A minervame6A minervame6B minervame6C minervame6D minervame6E minervame6F minervame6I minervame6G minervame6H minervame6I minervame6J
-for playlist in minervame1E
+for playlist in minervame6A
 do 
     echo $playlist
-    find "${PWD}" /pnfs/minerva/persistent/MAD_ntuples/production_p1/MuonKuldged/Neutrinos/FullDetector/Merged_mc_ana_me1E_FirstProductionAnatuples_DualVertex/ -name 'MasterAnaDev_mc_AnaTuple_run*.root' > ${playlist}_mc_DualVertex_FullDetector.txt
-    #xrdify $playlist.txt > MasterAnaDev_MC_${playlist}_MuonKludged.txt
+    # MC
+    find "${PWD}" /pnfs/minerva/persistent/users/zdar/Merged_mc_ana_me6A_DualVertex_p3/ -name 'MasterAnaDev_mc_AnaTuple_run*.root' > ${playlist}_mc_DualVertex_FullDetector_p3.txt
+    xrdify ${playlist}_mc_DualVertex_FullDetector_p3.txt > MasterAnaDev_MC_${playlist}.txt
+    # Data
+    find "${PWD}" /pnfs/minerva/persistent/users/zdar/Merged_data_ana_me6A_DualVertex_p3/ -name 'MasterAnaDev_data_AnaTuple_run*.root' > ${playlist}_data_DualVertex_p3.txt
+    xrdify ${playlist}_data_DualVertex_p3.txt > MasterAnaDev_Data_${playlist}.txt
+
 done
