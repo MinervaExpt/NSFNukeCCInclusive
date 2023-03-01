94a6a0122e80d75fe98f6dd5d77cca628f053948
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2023-03-01 04:17:29 -0600

    Updated to read from data preservation area.

--- ana/include/playlists/readtuples.sh
+++ ana/include/playlists/readtuples.sh
@@ -6,10 +6,10 @@ for playlist in minervame6A
 do 
     echo $playlist
     # MC
-    find "${PWD}" /pnfs/minerva/persistent/users/zdar/Merged_mc_ana_me6A_DualVertex_p3/ -name 'MasterAnaDev_mc_AnaTuple_run*.root' > ${playlist}_mc_DualVertex_FullDetector_p3.txt
+    find "${PWD}" /pnfs/minerva/persistent/DataPreservation/p3/FullDetector/Merged_mc_ana_me6A_DualVertex_p3/ -name 'MasterAnaDev_mc_AnaTuple_run*.root' > ${playlist}_mc_DualVertex_FullDetector_p3.txt
     xrdify ${playlist}_mc_DualVertex_FullDetector_p3.txt > MasterAnaDev_MC_${playlist}.txt
     # Data
-    find "${PWD}" /pnfs/minerva/persistent/users/zdar/Merged_data_ana_me6A_DualVertex_p3/ -name 'MasterAnaDev_data_AnaTuple_run*.root' > ${playlist}_data_DualVertex_p3.txt
+    find "${PWD}" /pnfs/minerva/persistent/DataPreservation/p3/Data/Merged_data_ana_me6A_DualVertex_p3/ -name 'MasterAnaDev_data_AnaTuple_run*.root' > ${playlist}_data_DualVertex_p3.txt
     xrdify ${playlist}_data_DualVertex_p3.txt > MasterAnaDev_Data_${playlist}.txt
 
 done
