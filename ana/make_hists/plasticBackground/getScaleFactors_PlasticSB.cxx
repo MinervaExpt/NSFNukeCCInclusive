b94cb1d64d615500bb28a9f1d50930110701dddb
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2023-03-02 05:10:29 -0600

    Changed to pTmu1D.

--- ana/make_hists/plasticBackground/getScaleFactors_PlasticSB.cxx
+++ ana/make_hists/plasticBackground/getScaleFactors_PlasticSB.cxx
@@ -122,7 +122,7 @@ int main(int argc, char * argv[]){
   vars.push_back("planeDNN");
   vars.push_back("Enu");
   vars.push_back("x");
-  vars.push_back("pTmu");
+  vars.push_back("pTmu1D");
   vars.push_back("pZmu1D");
   vars.push_back("ThetamuDeg");
   
