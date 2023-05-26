a17d0659e8ab701299b462792ae6ba6c26a4556b
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2023-05-26 06:55:01 -0500

    Removed nonres production reduction AND deuterium genie pi tune.

--- ana/make_hists/runEfficiency.cxx
+++ ana/make_hists/runEfficiency.cxx
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]){
   PlotUtils::MinervaUniverse::SetNuEConstraint(true);
   PlotUtils::MinervaUniverse::SetPlaylist(plist_string);
   PlotUtils::MinervaUniverse::SetAnalysisNuPDG(-14);
-  PlotUtils::MinervaUniverse::SetNonResPiReweight(true);
+  PlotUtils::MinervaUniverse::SetNonResPiReweight(false);
   PlotUtils::MinervaUniverse::SetNFluxUniverses(100);
   PlotUtils::MinervaUniverse::SetDeuteriumGeniePiTune(true);
   PlotUtils::MinervaUniverse::SetZExpansionFaReweight(false);
