fe95dfd73765472d1f7d21095934e6864fed78de
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2023-06-07 13:52:57 -0500

    Plotting W and Q2.

---
+++ ana/plotting/plotCVError_WQ2.py
@@ -0,0 +1,296 @@
+import ROOT
+import os,sys
+from ROOT import PlotUtils
+from ROOT import gStyle
+
+ROOT.gROOT.SetBatch(True)
+
+dirpwd = sys.argv[1]
+targetID = sys.argv[2] 
+targetZ = sys.argv[3]
+plist = sys.argv[4]
+scale = sys.argv[5]
+
+infile= ROOT.TFile(str(dirpwd)+"/EventSelection_%s_t%s_z%02s_sys.root"%(plist, targetID, targetZ))
+#if syss=="False":
+#    infile= ROOT.TFile(str(dirpwd)+"EventSelection_%s_t%s_z%02s_nosys.root"%(plist, targetID, targetZ))
+
+canvas1 = ROOT.TCanvas() # have to declare canvas before calling mnvplotter :))
+mnv = PlotUtils.MnvPlotter()
+
+mcPOT = infile.Get("MCPOT").GetVal()
+dataPOT = infile.Get("DataPOT").GetVal()
+
+mcScale = dataPOT/mcPOT
+if scale == "1":
+    mcScale = 1
+
+mat = None
+trueZ = None
+
+if targetZ == "26":
+    trueZ = "Iron"
+    mat = "Fe"
+
+if targetZ == "82":
+    trueZ = "Lead"
+    mat = "Pb"
+
+if targetZ == "06":
+    trueZ = "Carbon"
+    mat = "C"
+
+if targetZ == "99":
+    trueZ = "Tracker"
+    mat = "CH"
+
+vars = ["W", "Q2"]#, "ThetamuDeg"]
+
+for var in vars:
+
+    mc_hist = infile.Get("selected_mc_reco_%s"%var)
+    data_hist = infile.Get("selected_data_reco_%s"%var)
+    bkg_mc = infile.Get("selected_mc_reco_bkg_%s"%var)
+
+    if var == "W":
+        mc_hist.GetXaxis().SetTitle("Reconstructed W (GeV)")
+        mc_hist.GetYaxis().SetTitle("Events/GeV")
+
+    if var == "Q2":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Q^{2}  GeV^{2}")
+        mc_hist.GetYaxis().SetTitle("Events/GeV^{2}")
+
+    if var == "Enu":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Antineutrino Energy (GeV)")
+        mc_hist.GetYaxis().SetTitle("Events/GeV")
+
+    if var == "x":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Bjorken x")
+        mc_hist.GetYaxis().SetTitle("Events (norm.)")
+
+    if var == "ThetamuDeg":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Muon Angle (Deg)")
+        mc_hist.GetYaxis().SetTitle("Events/Deg")
+    
+    if var == "pTmu1D":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Muon p_{T} (GeV/c)")
+        mc_hist.GetYaxis().SetTitle("Events/(GeV/c)")
+
+    if var == "pZmu1D":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Muon p_{Z} (GeV/c)")
+        mc_hist.GetYaxis().SetTitle("Events/(GeV/c)")
+
+    '''
+    if myvariable == "xfine":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Bjorken x")
+        mc_hist.GetYaxis().SetTitle("Events (norm.)")
+
+
+    if myvariable == "xBrian":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Bjorken x")
+        mc_hist.GetYaxis().SetTitle("Events (norm.)")
+
+    if myvariable == "x09":
+        mc_hist.GetXaxis().SetTitle("Reconstructed Bjorken x")
+        mc_hist.GetYaxis().SetTitle("Events (norm.)")
+    ''' 
+    mc_hist.GetXaxis().CenterTitle()
+    mc_hist.GetYaxis().CenterTitle()
+    #print(data_hist.GetBinContent(data_hist.GetNbinsX())) # last bin
+    #print(data_hist.GetBinContent(data_hist.GetNbinsX()+1)) # overflow
+    #mc_hist.GetXaxis().SetRangeUser(0,0.001)
+
+    #mnv.ApplyStyle(1)
+    mnv.DrawDataMCWithErrorBand(data_hist, mc_hist, mcScale, "TR", False, None, None, False, True, False)
+
+    #mnv.AddChi2Label(data_hist, mc_hist, mcScale, "TL", 0.035, 0.0, True, False)
+
+    '''
+    Chi2DataMC( dataHist, mcHist, ndf, mcScale, useDataErrorMatrix, useOnlyShapeErrors)
+    If your data distribution has ONLY STATISTICAL uncertainties, useDataErrorMatrix should be FALSE
+    If you are using POT-normalization, then useOnlyShapeErrors should be FALSE
+    '''
+    if targetZ == "99":
+        mnv.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+    else:
+        mnv.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+    mnv.AddPOTNormBox(dataPOT,mcPOT, 0.3, 0.82)
+
+    canvas1.SetLogx(False)
+    if var == "Q2":
+        canvas1.SetLogx()
+    canvas1.Modified()
+    canvas1.Print("EventSelection_t%s_z%02s_%s_%s.png"%(targetID, targetZ, var, plist))
+
+
+    # ----------------------------------------------------------------------------
+    # Event selection with simulated background
+
+    mnv.DrawDataMCWithErrorBand(data_hist, mc_hist, mcScale, "TR", False, bkg_mc, None, False, True, False)
+    #mnv.AddChi2Label(data_hist, mc_hist, mcScale, "TL", 0.035, 0.0, True, False)
+    if targetZ == "99":
+        mnv.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+    else:
+        mnv.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+    mnv.AddPOTNormBox(dataPOT,mcPOT, 0.3, 0.82)
+    canvas1.Print("EventSelection_t%s_z%02s_%s_%s_wsimbkg.png"%(targetID, targetZ, var, plist))
+
+
+    # ----------------------------------------------------------------------------
+
+    # Fractional Error Groups
+    mnv2 = PlotUtils.MnvPlotter()
+    mnv2.ApplyStyle(7)
+
+    mnv2.error_summary_group_map.clear()
+    mnv2.error_summary_group_map["Muon Energy"].push_back("Muon_Energy_Resolution")
+    mnv2.error_summary_group_map["Muon Energy"].push_back("Muon_Energy_MINOS")
+    mnv2.error_summary_group_map["Muon Energy"].push_back("Muon_Energy_MINERvA")
+
+    mnv2.error_summary_group_map["Muon Efficiency"].push_back("MINOS_Reconstruction_Efficiency")
+
+    mnv2.error_summary_group_map["Muon Angle"].push_back("BeamAngleX")
+    mnv2.error_summary_group_map["Muon Angle"].push_back("BeamAngleY")
+
+    mnv2.error_summary_group_map["Flux"].push_back("Flux")
+
+    mnv2.error_summary_group_map["Interaction Model"].push_back("RPA_HighQ2")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("RPA_LowQ2")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("Low_Recoil_2p2h_Tune")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_AhtBY")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_BhtBY")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_CCQEPauliSupViaKF")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_CV1uBY")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_CV2uBY")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_EtaNCEL")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_MaCCQE")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_MaNCEL")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_MaRES")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_MvRES")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_NormDISCC")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_NormNCRES")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_Rvn1pi")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_Rvn2pi")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_Rvp1pi")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_Rvp2pi")
+    mnv2.error_summary_group_map["Interaction Model"].push_back("GENIE_VecFFCCQEshape")
+
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_AGKYxF1pi")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_RDecBR1gamma")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_Theta_Delta2Npi")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrAbs_N")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrAbs_pi")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrCEx_N")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrCEx_pi")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrElas_N")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrElas_pi")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrInel_N")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrPiProd_N")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_FrPiProd_pi")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_MFP_N")
+    mnv2.error_summary_group_map["FSI"].push_back("GENIE_MFP_pi")
+
+    mnv2.error_summary_group_map["Hadrons"].push_back("response_em")
+    mnv2.error_summary_group_map["Hadrons"].push_back("response_low_proton")
+    mnv2.error_summary_group_map["Hadrons"].push_back("response_mid_proton")
+    mnv2.error_summary_group_map["Hadrons"].push_back("response_high_proton")
+    mnv2.error_summary_group_map["Hadrons"].push_back("response_meson")
+    mnv2.error_summary_group_map["Hadrons"].push_back("response_other")
+
+    mnv2.error_summary_group_map["Hadrons"].push_back("GEANT_Neutron")
+    mnv2.error_summary_group_map["Hadrons"].push_back("GEANT_Proton")
+    mnv2.error_summary_group_map["Hadrons"].push_back("GEANT_Pion")
+
+    mnv2.error_summary_group_map["Target Mass"].push_back("Target_Mass_C")
+    mnv2.error_summary_group_map["Target Mass"].push_back("Target_Mass_Fe")
+    mnv2.error_summary_group_map["Target Mass"].push_back("Target_Mass_Pb")
+    mnv2.error_summary_group_map["Target Mass"].push_back("Target_Mass_CH")
+    mnv2.error_summary_group_map["Target Mass"].push_back("Target_Mass_H2O")
+    
+    # Change colour
+    mnv2.error_color_map["Target Mass"] = 15
+    mnv2.error_color_map["Muon Efficiency"] = 46
+    mnv2.error_color_map["Particle Response"] = 65
+    
+    #mnv2.ApplyStyle(7)
+    mnv2.DrawErrorSummary(mc_hist, "TL", True, True, 0.0, False, "",True);
+    # last boolean decides whether frac or not
+
+    keys = canvas1.GetListOfPrimitives();
+    for k in keys:
+        if(k.ClassName().find("TH1")!=-1):
+            if var == "Enu":
+                k.GetYaxis().SetRangeUser(0,k.GetMaximum()*1.02) # to 0.35 for Enu
+            if var == "x":
+                k.GetYaxis().SetRangeUser(0,k.GetMaximum()*1.05)
+            if var == "pTmu1D":
+                k.GetYaxis().SetRangeUser(0,k.GetMaximum()*1.05)
+            if var == "pZmu1D":
+                k.GetYaxis().SetRangeUser(0,k.GetMaximum()* 1.05)
+            if var == "ThetamuDeg":
+                k.GetYaxis().SetRangeUser(0,k.GetMaximum()*1.05)
+
+        if(k.ClassName().find("Legend")!=-1):
+            if var == "Enu":
+                k.SetNColumns(2)
+                k.SetX2(20) #Enu
+                k.SetY1(0.15) #Enu
+                if targetZ == "99":
+                    k.SetY1(0.14) #Enu
+
+            if var == "x":
+                k.SetNColumns(2)
+                k.SetX2(0.5) #x
+                k.SetY1(0.16) #x
+                if targetZ == "99":
+                    k.SetY1(0.18) #Enu
+
+            if var == "pTmu1D":
+                k.SetNColumns(2)
+                k.SetX2(2.5) 
+                k.SetY1(0.17) #Enu
+                if targetZ == "99":
+                    k.SetY1(0.16) #Enu
+                if targetZ == "82":
+                    k.SetY1(0.18) #Enu
+                    if targetID == "3":
+                       k.SetY1(0.24) #Enu 
+                    if targetID == "5":
+                        k.SetY1(0.22) #Enu 
+                if targetZ == "06":
+                    k.SetY1(0.18) #Enu
+
+            if var == "pZmu1D":
+                k.SetNColumns(2)
+                k.SetX2(20)
+                if targetZ == "26":
+                    k.SetY1(0.16) #Enu
+                if targetZ == "26":
+                    k.SetY1(0.16) #Enu
+                if targetZ == "99":
+                    k.SetY1(0.15) #Enu
+        
+            if var == "ThetamuDeg":
+                k.SetNColumns(2)
+                k.SetX2(17) 
+                k.SetY1(0.16)
+                if targetZ == "99":
+                    k.SetY1(0.14) #Enu
+                if targetZ == "06":
+                    k.SetY1(0.15) #Enu
+                if targetZ == "82":
+                    k.SetY1(0.15)
+
+
+    if targetZ == "99":
+        mnv2.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+    else:
+        mnv2.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+
+    canvas1.Modified()
+    canvas1.Print("EventSelection_t%s_z%02s_%s_%s_FracErr.png"%(targetID, targetZ, var, plist))
+
+
+data_hist.SetDirectory(0)
+mc_hist.SetDirectory(0)
+print("DONE %s %s %02s"%(plist, targetID, targetZ))
---
+++ ana/plotting/plotMigrationMatrix_WQ2.py
@@ -0,0 +1,298 @@
+import ROOT
+import os,sys
+from ROOT import PlotUtils
+from ROOT import gStyle
+
+ROOT.gROOT.SetBatch(True)
+
+def colNormalize(hist):
+        norm_hist = hist.Clone()
+        norm_hist.Reset()
+
+        nXBins = hist.GetNbinsX()+2 # to add overflow + underflow
+        nYBins = hist.GetNbinsY()+2 # to add overflow + underflow
+        
+        for i in range(0, nXBins): # column
+                col_integral = 0
+                for j in range(0,nYBins): # row
+                        col_integral += hist.GetBinContent(i,j)
+                for j in range(0,nYBins):
+                        if col_integral==0: norm_hist.SetBinContent(i,j,0)
+                        else: norm_hist.SetBinContent(i,j, hist.GetBinContent(i,j)*100/col_integral)
+        return norm_hist
+
+
+def rowNormalize(hist):
+        norm_hist = hist.Clone()
+        norm_hist.Reset()
+
+        nXBins = hist.GetNbinsX()+2 # to add overflow + underflow
+        nYBins = hist.GetNbinsY()+2 # to add overflow + underflow
+        
+        for i in range(0, nYBins): # column
+                row_integral = 0
+                for j in range(0,nXBins): # row
+                        row_integral += hist.GetBinContent(j,i)
+                for j in range(0,nXBins):
+                        if row_integral==0: norm_hist.SetBinContent(j,i,0)
+                        else: norm_hist.SetBinContent(j,i, hist.GetBinContent(j,i)*100/row_integral)
+        return norm_hist
+
+
+ROOT.TH1.AddDirectory(False)
+dirpwd = sys.argv[1]
+targetID = sys.argv[2] 
+targetZ = sys.argv[3]
+plist = sys.argv[4]
+
+infile= ROOT.TFile(str(dirpwd)+"/Migration_%s_t%s_z%02s_sys.root"%(plist, targetID, targetZ))
+canvas1 = ROOT.TCanvas() # have to declare canvas before calling mnvplotter :))
+mnv = PlotUtils.MnvPlotter()
+
+mat = None
+trueZ = None
+
+if targetZ == "26":
+    trueZ = "Iron"
+    mat = "Fe"
+
+if targetZ == "82":
+    trueZ = "Lead"
+    mat = "Pb"
+
+if targetZ == "06":
+    trueZ = "Carbon"
+    mat = "C"
+
+if targetZ == "99":
+    trueZ = "Tracker"
+    mat = "CH"
+
+vars = ["W", "Q2"] #"ThetamuDeg"]
+
+for var in vars:
+        print("---------------------------------------")
+        print(var)
+        mc_hist = infile.Get("response1d_%s_migration"%var)
+        new = infile.Get("selected_Migration_%s"%var)
+
+        # to add overflow + underflow
+        mc_hist.GetXaxis().SetRangeUser(-1,mc_hist.GetNbinsX()+1)
+        mc_hist.GetYaxis().SetRangeUser(-1,mc_hist.GetNbinsY()+1)
+
+        gStyle. SetPalette(1)
+        gStyle.SetOptTitle(0)
+        gStyle.SetPaintTextFormat("2.0f")
+
+        if var == "W":
+                mc_hist.GetXaxis().SetTitle("Reconstructed W bin number")
+                mc_hist.GetYaxis().SetTitle("True W bin number")
+                mc_hist.SetMarkerSize(1.2)
+                new.GetXaxis().SetTitle("Reconstructed W (GeV)")
+                new.GetYaxis().SetTitle("True W (GeV)")
+                new.SetMarkerSize(1.2)
+
+        if var == "Q2":
+                mc_hist.GetXaxis().SetTitle("Reconstructed Q^{2} bin number")
+                mc_hist.GetYaxis().SetTitle("True Q^{2} bin number")
+                mc_hist.SetMarkerSize(1.2)
+                new.GetXaxis().SetTitle("Reconstructed Q^{2}  GeV^{2}")
+                new.GetYaxis().SetTitle("True Q^{2} GeV^{2}")
+                new.SetMarkerSize(1.2)
+
+
+
+        if var == "Enu":
+                mc_hist.GetXaxis().SetTitle("Reconstructed E_{#bar{#nu}} bin number")
+                mc_hist.GetYaxis().SetTitle("True E_{#bar{#nu}} bin number")
+                mc_hist.SetMarkerSize(1.2)
+                new.GetXaxis().SetTitle("Reconstructed E_{#bar{#nu}}")
+                new.GetYaxis().SetTitle("True E_{#bar{#nu}}")
+                new.SetMarkerSize(1.2)
+
+
+        if var == "x":
+                mc_hist.GetXaxis().SetTitle("Reconstructed Bjorken x bin number")
+                mc_hist.GetYaxis().SetTitle("True Bjorken x bin number")
+                mc_hist.SetMarkerSize(1.5)
+                new.GetXaxis().SetTitle("Reconstructed Bjorken x")
+                new.GetYaxis().SetTitle("True Bjorken x")
+                new.SetMarkerSize(1.2)
+        
+        if var == "pTmu1D":
+                mc_hist.GetXaxis().SetTitle("Reconstructed Muon p_{T} bin number")
+                mc_hist.GetYaxis().SetTitle("True Muon p_{T} bin number")
+                mc_hist.SetMarkerSize(1.5)
+                new.GetXaxis().SetTitle("Reconstructed Muon p_{T} (GeV/c)")
+                new.GetYaxis().SetTitle("True Muon p_{T} (GeV/c)")
+                new.SetMarkerSize(1.2)
+                
+        if var == "pZmu1D":
+                mc_hist.GetXaxis().SetTitle("Reconstructed Muon p_{Z} bin number")
+                mc_hist.GetYaxis().SetTitle("True Muon p_{Z} bin number")
+                mc_hist.SetMarkerSize(1.5)
+                new.GetXaxis().SetTitle("Reconstructed Muon p_{Z} (GeV/c)")
+                new.GetYaxis().SetTitle("True Muon p_{Z} (GeV/c)")
+                new.SetMarkerSize(1.2)
+
+        if var == "ThetamuDeg":
+                mc_hist.GetXaxis().SetTitle("Reconstructed Muon #theta_{#mu} bin number")
+                mc_hist.GetYaxis().SetTitle("True Muon #theta_{#mu} bin number")
+                mc_hist.SetMarkerSize(1.5)
+                new.GetXaxis().SetTitle("Reconstructed Muon #theta_{#mu} (Deg)")
+                new.GetYaxis().SetTitle("True Muon #theta_{#mu} (Deg)")
+                new.SetMarkerSize(1.2)
+
+
+        '''
+        if myvariable == "x09":
+                mc_hist.GetXaxis().SetTitle("Reconstructed Bjorken x Bins")
+                mc_hist.GetYaxis().SetTitle("True Bjorken x Bins")
+
+        if myvariable == "xfine":
+                mc_hist.GetXaxis().SetTitle("Reconstructed Bjorken x Bins")
+                mc_hist.GetYaxis().SetTitle("True Bjorken x Bins")
+
+        if myvariable == "xBrian":
+                mc_hist.GetXaxis().SetTitle("Reconstructed Bjorken x Bins")
+                mc_hist.GetYaxis().SetTitle("True Bjorken x Bins")
+        '''
+        mc_hist.GetXaxis().CenterTitle()
+        mc_hist.GetXaxis().SetTitleFont(42)
+        mc_hist.GetXaxis().SetTitleSize(0.05)
+
+        mc_hist.GetYaxis().SetTitleOffset(1)
+        mc_hist.GetYaxis().CenterTitle()
+        mc_hist.GetYaxis().SetTitleFont(42)
+        mc_hist.GetYaxis().SetTitleSize(0.05)
+
+        mc_hist.GetZaxis().CenterTitle()
+        mc_hist.GetZaxis().SetRangeUser(0,1000)
+        mc_hist.GetZaxis().SetTitleFont(42)
+        mc_hist.GetZaxis().SetTitleOffset(1.1)
+        mc_hist.GetZaxis().SetTitleSize(0.045)
+
+        new.GetXaxis().CenterTitle()
+        new.GetXaxis().SetTitleFont(42)
+        new.GetXaxis().SetTitleSize(0.05)
+
+        new.GetYaxis().SetTitleOffset(1)
+        new.GetYaxis().CenterTitle()
+        new.GetYaxis().SetTitleFont(42)
+        new.GetYaxis().SetTitleSize(0.05)
+
+        new.GetZaxis().CenterTitle()
+        new.GetZaxis().SetRangeUser(0,1000)
+        new.GetZaxis().SetTitleFont(42)
+        new.GetZaxis().SetTitleOffset(1.1)
+        new.GetZaxis().SetTitleSize(0.045)
+
+
+        '''
+        mc_hist.GetXaxis().SetBinLabel(0,'underflow')
+        mc_hist.GetXaxis().SetBinLabel(1,'0.0 - 0.05')
+        mc_hist.GetXaxis().SetBinLabel(2,'0.05 - 0.1')
+        mc_hist.GetXaxis().SetBinLabel(3,'0.1 - 0.2')
+        mc_hist.GetXaxis().SetBinLabel(4,'0.2 - 0.4')
+        mc_hist.GetXaxis().SetBinLabel(5,'0.4 - 1.0')
+        mc_hist.GetXaxis().SetBinLabel(6,'1.0 - 2.2')
+        mc_hist.GetXaxis().SetBinLabel(7,' > 2.2')
+        #{0.0, 0.05, 0.1, 0.2, 0.4, 1.0, 2.2}
+
+        mc_hist.GetYaxis().SetBinLabel(0,'underflow')
+        mc_hist.GetYaxis().SetBinLabel(1,'0.0 - 0.05')
+        mc_hist.GetYaxis().SetBinLabel(2,'0.05 - 0.1')
+        mc_hist.GetYaxis().SetBinLabel(3,'0.1 - 0.2')
+        mc_hist.GetYaxis().SetBinLabel(4,'0.2 - 0.4')
+        mc_hist.GetYaxis().SetBinLabel(5,'0.4 - 1.0')
+        mc_hist.GetYaxis().SetBinLabel(6,'1.0 - 2.2')
+        mc_hist.GetYaxis().SetBinLabel(7,' > 2.2')
+        '''
+
+        mc_hist.GetXaxis().SetTitleSize(0.05)
+        mc_hist.GetYaxis().SetTitleSize(0.05)
+        mc_hist.GetYaxis().SetTitleOffset(1.)
+        mc_hist.GetZaxis().SetTitleSize(0.04)
+        mc_hist.GetZaxis().SetTitleOffset(1.2)
+        mc_hist.GetZaxis().SetLabelSize(0.04)
+
+        new.GetXaxis().SetTitleSize(0.05)
+        new.GetYaxis().SetTitleSize(0.05)
+        new.GetYaxis().SetTitleOffset(1.)
+        new.GetZaxis().SetTitleSize(0.04)
+        new.GetZaxis().SetTitleOffset(1.2)
+        new.GetZaxis().SetLabelSize(0.04)
+
+        # SIMPLE OCCUPANCY
+        mc_hist.Draw("COLZTEXT")
+        if targetZ == "99":
+                mnv.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+        else:
+                mnv.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+        mnv.SetRedHeatPalette()
+        #mnv.SetROOT6Palette(109)
+        mc_hist.GetZaxis().SetTitle("Event Rate")
+        canvas1.Print("Migration_%s_t%s_z%02s_%s_Occupancy.png"%(plist, targetID, targetZ, var))
+
+        new.Draw("COLZ")
+        if targetZ == "99":
+                mnv.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+        else:
+                mnv.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+        new.GetZaxis().SetTitle("Event Rate")
+        canvas1.Print("Migration_%s_t%s_z%02s_%s_Occupancy_Binned.png"%(plist, targetID, targetZ, var))
+
+        # COLUMN NORMALIZED
+        mc_hist.GetZaxis().SetRangeUser(0,100)
+        mc_hist.GetZaxis().SetTitle("Column Normalized Event Rate (%)")
+        colnorm_hist = colNormalize(mc_hist)
+        colnorm_hist.Draw("COLZTEXT")
+        if targetZ == "99":
+                mnv.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+        else:
+                mnv.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+        canvas1.Print("Migration_%s_t%s_z%02s_%s_ColumnNorm.png"%(plist, targetID, targetZ, var))
+
+
+        new.GetZaxis().SetRangeUser(0,100)
+        new.GetZaxis().SetTitle("Column Normalized Event Rate (%)")
+        colnorm_hist_new = colNormalize(new)
+        colnorm_hist_new.Draw("COLZ")
+        if targetZ == "99":
+                mnv.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+        else:
+                mnv.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+        canvas1.Print("Migration_%s_t%s_z%02s_%s_ColumnNorm_Binned.png"%(plist, targetID, targetZ, var))
+
+        # ROW NORMALIZED
+        mc_hist.GetZaxis().SetTitle("Row Normalized Event Rate (%)")
+        rownorm_hist = rowNormalize(mc_hist)
+        rownorm_hist.Draw("COLZTEXT")
+        if targetZ == "99":
+                mnv.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+        else:
+                mnv.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+        canvas1.Print("Migration_%s_t%s_z%02s_%s_RowNorm.png"%(plist, targetID, targetZ, var))
+
+        new.GetZaxis().SetTitle("Row Normalized Event Rate (%)")
+        rownorm_hist_new = rowNormalize(new)
+        rownorm_hist_new.Draw("COLZ")
+        if targetZ == "99":
+                mnv.AddHistoTitle("%s"%(trueZ), 0.05, 1)
+        else:
+                mnv.AddHistoTitle("Target %s %s"%(targetID, trueZ), 0.05, 1)
+        canvas1.Print("Migration_%s_t%s_z%02s_%s_RowNorm_Binned.png"%(plist, targetID, targetZ, var))
+
+#mnv.DrawNormalizedMigrationHistogram(mc_hist, True, False, True, False)
+'''
+void MnvPlotter::DrawNormalizedMigrationHistogram(
+        const TH2D* h_migration,
+        const bool drawAsMatrix,
+        const bool coarseContours, /* = false */
+        const bool includeFlows, /* = true */
+        const bool noText /* = false */
+        )
+{
+'''
+
+print("DONE %s %s %02s"%(plist, targetID, targetZ))
