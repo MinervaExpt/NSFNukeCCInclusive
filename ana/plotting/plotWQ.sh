6f9d32e2749bc08abe2eb9f3f11385449f7c49f2
Anezka Klustova <ane.klustova.16@ucl.ac.uk>
2023-06-11 22:42:34 -0500

    Plotting all WQ event selections and migrations.

---
+++ ana/plotting/plotWQ.sh
@@ -0,0 +1,26 @@
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 2 26 minervame6A 0
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 3 26 minervame6A 0
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 5 26 minervame6A 0
+
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 2 82 minervame6A 0
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 3 82 minervame6A 0
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 4 82 minervame6A 0
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 5 82 minervame6A 0
+
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 3 06 minervame6A 0
+
+python plotCVError_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 99 99 minervame6A 0
+
+
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 2 26 minervame6A
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 3 26 minervame6A
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 5 26 minervame6A
+
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 2 82 minervame6A
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 3 82 minervame6A
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 4 82 minervame6A
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 5 82 minervame6A
+
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 3 06 minervame6A
+
+python plotMigrationMatrix_WQ2.py /minerva/data/users/anezkak/06-05-2023_v1-nonrespi/WQ2_ME6A 99 99 minervame6A
